a4482a392ced24326103b436dcf1b92e87a615e0
Patricia Atanasoff <system.informatica365@gmail.com>
2023-09-09 20:38:28 -0300

    header, nav, logo, dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {colors: {
+      primary: '#0f172a',
+    },
+    fontFamily: {
+      roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+    },
+  },
   },
   plugins: [],
 }
