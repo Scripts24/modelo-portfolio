a55cd461f5f6a3500e192449221b783465d96f19
Patricia Atanasoff <system.informatica365@gmail.com>
2023-09-14 00:12:16 -0300

    upload

--- dist/app.js
+++ dist/app.js
@@ -65,4 +65,26 @@ darkButton.addEventListener('click', function () {
 
 });
 
+let boton = document.querySelector('.top')
+
+window.onscroll = function(){
+    
+    //Botón Top
+
+    if(document.documentElement.scrollTop > 250){
+        document.querySelector('.top ').style.display='block'
+    }
+    else{
+        document.querySelector('.top ').style.display='none' 
+    }
+
+    boton.addEventListener('click', function(){
+        window.scrollTo({
+            top:0,
+            behavior: 'smooth'
+        })
+    })
+
+}
+
 
